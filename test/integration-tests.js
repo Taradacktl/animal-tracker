00a33b265232085ac2787d3bc4428c01d7de6f77
Taradacktl <35745484+Taradacktl@users.noreply.github.com>
2018-05-05 15:38:59 -0700

    Update integration-tests.js

--- test/integration-tests.js
+++ test/integration-tests.js
@@ -1,18 +1,24 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-//const mongoose = require('mongoose');
-//const faker = require('faker');
-//const {DATABASE_URL, TEST_DATABASE_URL} = //require('../config');
-//const {Raid} = require('../models/raid-model');
-//const {User} = require('../models/user-model');
-const {closeServer, runServer, app} = require('../server');
 
+const {app, runServer, closeServer} = require('../server');
+
+const expect = chai.expect;
 chai.should();
 chai.use(chaiHttp);
 
 describe('Root URL', function() {
-    it('should respond with a status of 200 and HTML', function() {
-      return chai.request(app)
+
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  it('should respond with a status of 200 and HTML', function() {
+    return chai.request(app)
         .get('/')
         .then(function(result) {
           result.should.have.status(200);
