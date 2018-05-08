cb7077ca8affe272b2edaf512319f2f74474fc7d
Tara Ragsdale <Taradacktl@aol.com>
2018-05-08 14:53:20 -0700

    update app.js

--- public/app.js
+++ public/app.js
@@ -4,28 +4,28 @@ var MOCK_STATUS_UPDATES = {
 	"statusUpdates": [
         {
             "id": "1111111",
-            "text": "Can't believe how much fun I'm having.",
+            "species": "Can't believe how much fun I'm having.",
             "friendId": "aaaaaa",
             "friendName": "John Doe",
             "publishedAt": 1470016976609
         },
         {
             "id": "2222222",
-            "text": "Have FOMO? Well you SHOULD!",
+            "species": "Have FOMO? Well you SHOULD!",
             "friendId": "bbbbbbb",
             "friendName": "Jane Doe",
             "publishedAt": 1470012976609
         },
         {
             "id": "333333",
-            "text": "They're giving out immortality and free $$$ where I am.",
+            "species": "They're giving out immortality and free $$$ where I am.",
             "friendId": "cccc",
             "friendName": "Jim Doe",
             "publishedAt": 1470011976609
         },
         {
             "id": "4444444",
-            "text": "humble brag humble brag humble brag",
+            "species": "humble brag humble brag humble brag",
             "friendId": "ddddd",
             "friendName": "Jackie Doe",
             "publishedAt": 1470009976609
@@ -50,7 +50,7 @@ function getRecentStatusUpdates(callbackFn) {
 function displayStatusUpdates(data) {
     for (index in data.statusUpdates) {
 	   $('body').append(
-        '<p>' + data.statusUpdates[index].text + '</p>');
+        '<p>' + data.statusUpdates[index].species + '</p>');
     }
 }
 
