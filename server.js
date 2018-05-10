df94f83aa6018c6f5740730572193471425f49c2
Taradacktl <35745484+Taradacktl@users.noreply.github.com>
2018-05-10 10:19:05 -0700

    Update server.js

--- server.js
+++ server.js
@@ -91,6 +91,12 @@ const app = express();
 app.use(morgan('common'));
 app.use(express.json());
 
+app.use(express.static('public'));
+
+if (require.main === module) {
+  app.listen(process.env.PORT || 8080, function () {
+    console.info(`App listening on ${this.address().port}`);
+  });
 
 app.get('/posts', (req, res) => {
   AnimalTracker
