83c73b19cb453f9e64a10c77e7511099f4d508ab
Tara Ragsdale <Taradacktl@aol.com>
2018-05-10 08:18:34 -0700

    update package.json

--- public/app.js
+++ public/app.js
@@ -59,7 +59,7 @@ function displayStatusUpdates(data) {
     for (index in data.statusUpdates) {
 	   $('body').append(
         '<p>' + "Date:" + " " + data.statusUpdates[index].date + '</p>' +
-        '<p>' + "Time of  Day:" + " " + data.statusUpdates[index].timeOfDay + '</p>' +
+        '<p>' + "Time of Day:" + " " + data.statusUpdates[index].timeOfDay + '</p>' +
         '<p>' + "Species:" + " " + data.statusUpdates[index].species + '</p>' +
         '<p>' + "Activity:" + " " + data.statusUpdates[index].activity + '</p>' +
         '<p>' + "Location:" + " " + data.statusUpdates[index].location + '</p>');
