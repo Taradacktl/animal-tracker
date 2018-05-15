20b6ecda56be8578714927741caacfb9523fff9a
Tara Ragsdale <Taradacktl@aol.com>
2018-05-15 13:40:50 -0700

    update url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/animal-tracker';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/animal-tracker';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://my-new-user:user123@ds117250.mlab.com:17250/animal-tracker';
 exports.PORT = process.env.PORT || 8088;
