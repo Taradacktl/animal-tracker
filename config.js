be8c68d27c82be8b244473e4a6187a98b860ae37
Tara Ragsdale <Taradacktl@aol.com>
2018-05-15 13:35:36 -0700

    -test

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/animal-tracker';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-animal-tracker';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/animal-tracker';
 exports.PORT = process.env.PORT || 8088;
