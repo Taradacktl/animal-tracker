2e5f099ad27cdfa7ad379d6339ba47020cc3498d
Taradacktl <35745484+Taradacktl@users.noreply.github.com>
2018-05-18 14:40:42 -0700

    Update routes.js

--- users/routes.js
+++ users/routes.js
@@ -2,8 +2,23 @@ const express = require('express'); const bodyParser = require('body-parser');
 
 const router = express.Router(); const jsonParser = bodyParser.json();
 
-router.post('/login', (req, res) => { res.status(200).json({message:'OK'}) });
+//user login
+router.post('/login', (req, res) => { 
+    const requiredFields = ['name', 'emailAddress'];
+    for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    res.status(200).json({message:'OK'})
+     }
+ }
+    const user = AnimalTracker.create(req.body.name, req.body.emailAddress);
+    res.status(200).json(user)
+ });
 
+//create new user
 router.post('/create', (req, res) => {
     let name = req.body.name;
     name = name.trim();
