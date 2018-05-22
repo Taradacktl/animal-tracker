1eb89f9fadb83d09b6ca36bf7fc5d956772bf233
Tara Ragsdale <Taradacktl@aol.com>
2018-05-22 16:04:31 -0700

    jwt tests

--- test/users-test.js
+++ test/users-test.js
@@ -55,8 +55,198 @@ describe('animal tracker API resource', function () {
       });
 
     });
-
+    describe('/login', function () {
+      it('Should reject requests with no credentials', function () {
+        return chai
+          .request(app)
+          .post('/users/login')
+          .then(() =>
+            expect.fail(null, null, 'Request should not succeed')
+          )
+          .catch(err => {
+            if (err instanceof chai.AssertionError) {
+              throw err;
+            }
+  
+            const res = err.response;
+            expect(res).to.have.status(400);
+          });
+      });
+      it('Should reject requests with incorrect usernames', function () {
+        return chai
+          .request(app)
+          .post('/users/login')
+          .send({ username: 'wrongUsername', password })        
+          .then(() =>
+            expect.fail(null, null, 'Request should not succeed')
+          )
+          .catch(err => {
+            if (err instanceof chai.AssertionError) {
+              throw err;
+            }
+  
+            const res = err.response;
+            expect(res).to.have.status(401);
+          });
+      });
+      it('Should reject requests with incorrect passwords', function () {
+        return chai
+          .request(app)
+          .post('/users/login')
+          .send({ username, password: 'wrongPassword' })
+          .then(() =>
+            expect.fail(null, null, 'Request should not succeed')
+          )
+          .catch(err => {
+            if (err instanceof chai.AssertionError) {
+              throw err;
+            }
+  
+            const res = err.response;
+            expect(res).to.have.status(401);
+          });
+      });
+      it('Should return a valid auth token', function () {
+        return chai
+          .request(app)
+          .post('/users/login')
+          .send({ username, password })
+          .then(res => {
+            expect(res).to.have.status(200);
+            expect(res.body).to.be.an('object');
+            const token = res.body.authToken;
+            expect(token).to.be.a('string');
+            const payload = jwt.verify(token, JWT_SECRET, {
+              algorithm: ['HS256']
+            });
+            expect(payload.user).to.deep.equal({
+              username,
+              firstName,
+              lastName
+            });
+          });
+      });
+    });
+  
+    describe('/refresh', function () {
+      it('Should reject requests with no credentials', function () {
+        return chai
+          .request(app)
+          .post('/users/refresh')
+          .then(() =>
+            expect.fail(null, null, 'Request should not succeed')
+          )
+          .catch(err => {
+            if (err instanceof chai.AssertionError) {
+              throw err;
+            }
+  
+            const res = err.response;
+            expect(res).to.have.status(401);
+          });
+      });
+      it('Should reject requests with an invalid token', function () {
+        const token = jwt.sign(
+          {
+            username,
+            firstName,
+            lastName
+          },
+          'wrongSecret',
+          {
+            algorithm: 'HS256',
+            expiresIn: '7d'
+          }
+        );
+  
+        return chai
+          .request(app)
+          .post('/users/refresh')
+          .set('Authorization', `Bearer ${token}`)
+          .then(() =>
+            expect.fail(null, null, 'Request should not succeed')
+          )
+          .catch(err => {
+            if (err instanceof chai.AssertionError) {
+              throw err;
+            }
+  
+            const res = err.response;
+            expect(res).to.have.status(401);
+          });
+      });
+      it('Should reject requests with an expired token', function () {
+        const token = jwt.sign(
+          {
+            user: {
+              username,
+              firstName,
+              lastName
+            },
+            exp: Math.floor(Date.now() / 1000) - 10 // Expired ten seconds ago
+          },
+          JWT_SECRET,
+          {
+            algorithm: 'HS256',
+            subject: username
+          }
+        );
+  
+        return chai
+          .request(app)
+          .post('/users/refresh')
+          .set('authorization', `Bearer ${token}`)
+          .then(() =>
+            expect.fail(null, null, 'Request should not succeed')
+          )
+          .catch(err => {
+            if (err instanceof chai.AssertionError) {
+              throw err;
+            }
+  
+            const res = err.response;
+            expect(res).to.have.status(401);
+          });
+      });
+      it('Should return a valid auth token with a newer expiry date', function () {
+        const token = jwt.sign(
+          {
+            user: {
+              username,
+              firstName,
+              lastName
+            }
+          },
+          JWT_SECRET,
+          {
+            algorithm: 'HS256',
+            subject: username,
+            expiresIn: '7d'
+          }
+        );
+        const decoded = jwt.decode(token);
+  
+        return chai
+          .request(app)
+          .post('/users/refresh')
+          .set('authorization', `Bearer ${token}`)
+          .then(res => {
+            expect(res).to.have.status(200);
+            expect(res.body).to.be.an('object');
+            const token = res.body.authToken;
+            expect(token).to.be.a('string');
+            const payload = jwt.verify(token, JWT_SECRET, {
+              algorithm: ['HS256']
+            });
+            expect(payload.user).to.deep.equal({
+              username,
+              firstName,
+              lastName
+            });
+            expect(payload.exp).to.be.at.least(decoded.exp);
+          });
+      });
+    });
   });
-
 });
 
--- users/routes.js
+++ users/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const passport = require('passport');
 const bodyParser = require('body-parser');
-
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 const jsonParser = bodyParser.json();
 const { User } = require('./model');
@@ -12,7 +13,29 @@ User.create(
 //router.post('/login', (req, res) => { res.status(200).json({ message: 'OK' }) });
 
 //login user
-
+const createAuthToken = function(user) {
+    return jwt.sign({user}, config.JWT_SECRET, {
+      subject: user.username,
+      expiresIn: config.JWT_EXPIRY,
+      algorithm: 'HS256'
+    });
+  };
+  
+  const localAuth = passport.authenticate('local', {session: false});
+  router.use(bodyParser.json());
+  // The user provides a username and password to login
+  router.post('/login', localAuth, (req, res) => {
+    const authToken = createAuthToken(req.user.serialize());
+    res.json({authToken});
+  });
+  
+  const jwtAuth = passport.authenticate('jwt', {session: false});
+  
+  // The user exchanges a valid JWT for a new one with a later expiration
+  router.post('/refresh', jwtAuth, (req, res) => {
+    const authToken = createAuthToken(req.user);
+    res.json({authToken});
+  });
   
 
 //create new user
